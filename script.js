f5a4a71f61301f82b09ec16f8603da5d19a6af91
User1loveyou <angel.of.the.night11037@gmail.com>
2026-06-06 22:40:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const home = document.getElementById("home");
+const passwordScreen = document.getElementById("passwordScreen");
+const spotifyScreen = document.getElementById("spotifyScreen");
 
+const startBtn = document.getElementById("startBtn");
+const verifyBtn = document.getElementById("verifyBtn");
+const nextBtn = document.getElementById("nextBtn");
+
+startBtn.addEventListener("click", () => {
+
+    home.classList.remove("active");
+    passwordScreen.classList.add("active");
+
+});
+
+verifyBtn.addEventListener("click", () => {
+
+    const password =
+        document.getElementById("password")
+        .value
+        .trim();
+
+    if(password === "31/10/2024"){
+
+        passwordScreen.classList.remove("active");
+        spotifyScreen.classList.add("active");
+
+    }else{
+
+        document.getElementById("error")
+        .textContent =
+        "Data incorreta ❤️";
+
+    }
+
+});
+
+nextBtn.addEventListener("click", () => {
+
+    alert(
+        "Em breve aqui abrirá a carta ❤️"
+    );
+
+});
